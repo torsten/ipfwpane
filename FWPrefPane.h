119a7b7858b70c46c0a49bb716fbe4a9625b9ea9
Torsten Becker <torsten.becker@gmail.com>
2008-09-20 15:09:01 +0300

    Initial commit of the ipfw prefpane with an already pretty nice UI.

---
+++ FWPrefPane.h
@@ -0,0 +1,19 @@
+//
+//  FWPrefPane.h
+//  FWPrefPane
+//
+//  Created by Torsten Becker on 20.09.08.
+//  Copyright (c) 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+
+@interface FWPrefPane : NSPreferencePane 
+{
+
+}
+
+- (void) mainViewDidLoad;
+
+@end
---
+++ FWPrefPane.m
@@ -0,0 +1,18 @@
+//
+//  FWPrefPane.m
+//  FWPrefPane
+//
+//  Created by Torsten Becker on 20.09.08.
+//  Copyright (c) 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "FWPrefPane.h"
+
+
+@implementation FWPrefPane
+
+- (void) mainViewDidLoad
+{
+}
+
+@end
